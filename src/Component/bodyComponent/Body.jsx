451f0ac5211ca251de0a0348d8a3bf81b3425ad2
Ahmad <hamadagh777@gmail.com>
2019-12-23 18:52:05 +0100

    adding resume description

--- src/Component/bodyComponent/Body.jsx
+++ src/Component/bodyComponent/Body.jsx
@@ -1,19 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './body.css';
-import Title from './body-title-comp/Title';
-import Form from './body-form-comp/Form';
-import Description from './body-description-comp/Description';
+import axios from 'axios';
+import REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN from '../../config';
+import Resume from './body-description-comp/Resume';
 
 const Body = () => {
 
+    const [userName, setUserName] = useState({ userName: '' })
+    const [userData, setUserData] = useState({})
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    function getUserData() {
+        const axiosGitHubGraphQL = axios.create({
+            baseURL: 'https://api.github.com/graphql',
+            headers: {
+                Authorization: `bearer ${
+                    REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+                    }`,
+            },
+        });
+
+        const GET_USER = `{
+            user(login: "${userName.userName}") {
+              name
+              url
+              bio
+              createdAt
+              location
+              followers {
+                totalCount
+              }
+              repositories(first: 100) {
+                nodes {
+                  ... on Repository {
+                    name
+                    createdAt
+                    updatedAt
+                    description
+                    url
+                    languages(first: 50, orderBy: {field: SIZE, direction: DESC}) {
+                      edges {
+                        size
+                        node {
+                          name
+                        }
+                      }
+                    }
+                  }
+                }
+                totalCount
+              }
+            }
+          }`;
+
+
+
+        axiosGitHubGraphQL
+            .post('', { query: GET_USER })
+            .then(data => {
+                console.log(data)
+                setUserData(data)
+
+
+            })
+
+            .catch((e) => {
+
+                setError(e)
+
+            })
+            .finally(setTimeout(function () { setLoading(false); }, 3000))
+
+    }
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault()
+
+        getUserData()
+
+
+    }
+    const handleChange = (e) => {
+        setUserName({ userName: `${e.target.value}` })
+
+    }
+
+
+
     return (
         <div className="body">
-            <Title />
-            <Form />
-            <Description />
+
+            <div className="title">
+                <h2>My Github Resumé</h2>
+            </div>
+
+            <div className="form">
+                <div className="form-group">
+                    <p>Github username</p>
+                    <div className="form-input-group">
+                        <form onSubmit={handleSearchSubmit}>
+                            <input
+                                className="form-input"
+                                placeholder="John Doe"
+                                id="url"
+                                type="text"
+                                onChange={handleChange}
+                            />
+                            <button type="submit" className="submit-button">
+                                Generieren
+                        </button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+            <Resume userData={userData.data} loading={loading} />
         </div>
     )
 
 }
-
-export default Body
+export default Body;
---
+++ src/Component/bodyComponent/body-description-comp/Resume.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './resume.css'
+
+function Resume(props) {
+    console.log(props)
+
+
+    if (props.loading) {
+        return null
+
+    }
+    return (
+
+        <div className="description">
+
+            <span className="user-name">{props.userData.data.user.name}</span>
+            <span className="user-bio">{props.userData.data.user.bio}</span>
+            <span><a className="user-url" href="#">{props.userData.data.user.url}</a></span>
+            <p className="user-description">On Github since {props.userData.data.user.createdAt.substring(0, 10)}, {props.userData.data.user.name} is a developer based in {props.userData.data.user.location}, with {props.userData.data.user.repositories.totalCount} repositories and {props.userData.data.user.followers.totalCount} followers  </p>
+            <div className="user-langauages">
+                <h2>langauages</h2>
+            </div>
+            <div className="user-repos">
+                <h2>Popular Repositories</h2>
+                {
+                    props.userData.data.user.repositories.nodes.map((repo) => {
+                        return (
+                            <div className="user-repo" >
+                                <div className="repos-header">
+                                    <span className="repo-title">{repo.name}</span>
+                                    <span className="repo-date">{`[${repo.createdAt.substring(0, 10)}]-[${repo.updatedAt.substring(0, 10)}]`}</span>
+                                </div>
+                                <div className="repo-lang">
+                                    {repo.languages.edges.map((lang) => { return <span className="repo-lang-used">{lang.node.name}</span> })}
+                                </div>
+                                <div className="repo-description">
+                                    <p>This repositry has [xxx] stars and [xxx] forks. if you would like more information about this repository and my contributed code, please visit <a href={repo.url}>{repo.name}</a> on Github</p>
+                                </div>
+
+                                <hr></hr>
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Resume
