b9fedef1f84e6520fe300e4115c39bd81162c27b
Ahmad <hamadagh777@gmail.com>
2020-01-22 13:47:38 +0100

    ignoring config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './Component/headerComponent/Header';
 import Body from './Component/bodyComponent/Body';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Body />
     </div>
   );
